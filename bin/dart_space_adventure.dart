17d8dfaead8646705acdcbaf71b281a60ffe8581
Katie <25576005+stuttskl@users.noreply.github.com>
2021-01-12 15:25:40 -0800

    refactor work into functions

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -2,21 +2,44 @@ import 'package:dart_space_adventure/dart_space_adventure.dart'
     as dart_space_adventure;
 import 'dart:io';
 
-void main(List<String> arguments) {
+void printGreeting() {
   print('Welcome to the Solar System!');
   print('There are 8 planets to explore.');
+}
 
-  print('What is your name?');
-  String name = stdin.readLineSync();
+void printIntroduction(String name) {
   print(
       'Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
+}
 
-  print('Let\'s go on an adventure!');
-  print('Shall I randomly choose a planet for you to visit? (Y or N)');
-  String answer = stdin.readLineSync();
+String responseToPrompt(String prompt) {
+  print(prompt);
+  return stdin.readLineSync();
+}
 
-  print('Name the planet you would like to visit.');
-  String planetName = stdin.readLineSync();
-  print('Traveling to $planetName...\n');
-  print('Arrived at $planetName. A very cold planet, furthest from the sun.');
+void travel() {
+  String answer;
+
+  while (answer != 'Y' && answer != 'N') {
+    answer = responseToPrompt(
+        'Shall I randomly choose a planet for you to visit? (Y or N)');
+
+    if (answer == 'Y') {
+      print('Okay, traveling to Mercury...\n');
+    } else if (answer == 'N') {
+      print('Name the planet you would like to visit.');
+      final planetName = stdin.readLineSync();
+      print('Traveling to $planetName...\n'
+          'Arrived at $planetName. A very cold planet, furthest from the sun.');
+    } else {
+      print('Sorry, I didn\'t get that');
+    }
+  }
+}
+
+void main(List<String> arguments) {
+  printGreeting();
+  printIntroduction(responseToPrompt('What is your name?'));
+  print('Let\'s go on an adventure! \n');
+  travel();
 }
