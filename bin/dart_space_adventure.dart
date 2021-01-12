6e1e4d09d91a6af85f54df69b4e7ef3d6d7449bc
Katie <25576005+stuttskl@users.noreply.github.com>
2021-01-12 15:04:34 -0800

    printing in dart

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,17 +1,18 @@
-import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
+import 'package:dart_space_adventure/dart_space_adventure.dart'
+    as dart_space_adventure;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_space_adventure.calculate()}!');
-  Welcome to the Solar System!
-  There are 8 planets to explore.
-  What is your name?
-  Jane
-  Nice to meet you, Jane. My name is Eliza, I'm an old friend of Alexa.
-  Let's go on an adventure!
-  Shall I randomly choose a planet for you to visit? (Y or N)
-  N
-  Name the planet you would like to visit.
-  Neptune
-  Traveling to Neptune...
-  Arrived at Neptune. A very cold planet, furthest from the sun.
+  print('Welcome to the Solar System!');
+  print('There are 8 planets to explore.');
+  print('What is your name?');
+  print('Jane');
+  print(
+      'Nice to meet you, Jane. My name is Eliza, I\'m an old friend of Alexa.');
+  print('Let\'s go on an adventure!');
+  print('Shall I randomly choose a planet for you to visit? (Y or N)');
+  print('N');
+  print('Name the planet you would like to visit.');
+  print('Neptune');
+  print('Traveling to Neptune...');
+  print('Arrived at Neptune. A very cold planet, furthest from the sun.');
 }
