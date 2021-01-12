770de528d29dea481d9c235ca6c2189f5db502e8
Katie <25576005+stuttskl@users.noreply.github.com>
2021-01-12 15:49:48 -0800

    wrapped main program logic into space adventure lib

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,45 +1,5 @@
-import 'package:dart_space_adventure/dart_space_adventure.dart'
-    as dart_space_adventure;
-import 'dart:io';
-
-void printGreeting() {
-  print('Welcome to the Solar System!');
-  print('There are 8 planets to explore.');
-}
-
-void printIntroduction(String name) {
-  print(
-      'Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
-}
-
-String responseToPrompt(String prompt) {
-  print(prompt);
-  return stdin.readLineSync();
-}
-
-void travel() {
-  String answer;
-
-  while (answer != 'Y' && answer != 'N') {
-    answer = responseToPrompt(
-        'Shall I randomly choose a planet for you to visit? (Y or N)');
-
-    if (answer == 'Y') {
-      print('Okay, traveling to Mercury...\n');
-    } else if (answer == 'N') {
-      print('Name the planet you would like to visit.');
-      final planetName = stdin.readLineSync();
-      print('Traveling to $planetName...\n'
-          'Arrived at $planetName. A very cold planet, furthest from the sun.');
-    } else {
-      print('Sorry, I didn\'t get that');
-    }
-  }
-}
+import 'package:dart_space_adventure/dart_space_adventure.dart';
 
 void main(List<String> arguments) {
-  printGreeting();
-  printIntroduction(responseToPrompt('What is your name?'));
-  print('Let\'s go on an adventure! \n');
-  travel();
+  startSpaceAdventure();
 }
